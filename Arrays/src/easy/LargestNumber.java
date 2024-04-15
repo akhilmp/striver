9ff9f3f0cba2dc7919bc7b5cb0c1d7b940dfd5a3
Akhil <akhil@Akhils-MacBook-Air.local>
2024-04-15 19:18:13 +0530

    Second commit

--- Arrays/src/easy/LargestNumber.java
+++ Arrays/src/easy/LargestNumber.java
@@ -8,7 +8,7 @@ public class LargestNumber {
 		// TODO Auto-generated method stub
 		
 		int[] input= {3,1,5,6,7};
-
+		int[] input1= {3,1,5,6,7};
 		/*
 		 * 1.brute force approach
 		 * Sort the array and return arr[n-1];
@@ -17,7 +17,23 @@ public class LargestNumber {
 		
 		Arrays.sort(input);
 		System.out.println("Largest Number is "+input[input.length-1]);
+
+		/*
+		 *Now optimised approach is
+		 *assign large=input[0]
+		 *compare with all the elements and return the output
+		 *Here TC would be O(n) 
+		 */
 		
+		int large=input1[0];
+		
+		for(int i=0;i<input1.length;i++)
+		{
+			if(input1[i]>large)
+				large=input1[i];
+		}
+		System.out.println("Largest Number with optimised approach is "+large);
+
 	}
 
 }
